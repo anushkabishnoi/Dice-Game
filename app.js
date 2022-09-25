8fb3b4b80ba08f07c74e5a2643e4d489407bac4d
decapacitator <anushkabishnoi9@gmail.com>
2022-09-25 12:37:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-var random1 = Math.floor(Math.random()*6) + 1;
+var random1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage1 = "images/dice" + random1 + ".png";
 document.querySelector(".img1").setAttribute("src", randomDiceImage1);
 
-var random2 = Math.floor(Math.random()*6) + 1;
+var random2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "images/dice" + random2 + ".png";
-document.querySelector(".img2").setAttribute("src", randomDiceImage2)
+document.querySelector(".img2").setAttribute("src", randomDiceImage2);
 
-if(random1 > random2)
-    document.querySelector("h1").innerHTML = "🏆 Player 1 Wins 🏆" ;
+if (random1 > random2)
+  document.querySelector("h1").innerHTML = "🏆 Player 1 Wins 🏆";
+else if (random1 === random2)
+  document.querySelector("h1").innerHTML = "🚩 Tie! 🚩";
+else document.querySelector("h1").innerHTML = "🏆 Player 2 Wins 🏆";
 
-else if(random1 === random2)
-    document.querySelector("h1").innerHTML = "🚩 Tie! 🚩";
-
-else
-    document.querySelector("h1").innerHTML = "🏆 Player 2 Wins 🏆" ;
